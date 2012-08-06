d5c801015d7af537ee11d0ced0c4002640873c38
RuSTer124 <ruslan124@list.ru>
2012-08-06 17:13:20 +0300

    First

---
+++ rustjava-learning/Test/src/com/ruster/test/Test.java
@@ -0,0 +1,17 @@
+package com.ruster.test;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
