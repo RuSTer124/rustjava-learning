d0aa4711cd802a659af2d223d39207c5c3d4cb7f
RuSTer124 <ruslan124@list.ru>
2012-08-06 18:49:30 +0300

    test2 repeat

--- rustjava-learning/Test/src/com/ruster/test/Test.java
+++ rustjava-learning/Test/src/com/ruster/test/Test.java
@@ -10,8 +10,8 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello   World");
-
+		System.out.println("Hello     World");
+		System.out.println("Hello     World");
 	}
 
 }
